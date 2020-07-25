db21e709f40f7fcb02aa851db1878428f552db9c
tonkaew131 <40863067+tonkaew131@users.noreply.github.com>
2020-07-25 21:27:42 +0700

    Add fairy souls counter

--- main.py
+++ main.py
@@ -13,6 +13,8 @@ def GetKillMobsList(stats):
 # Check if folder is not exist
 if not os.path.exists('kills'):
     os.makedirs('kills')
+if not os.path.exists('etc'):
+    os.makedirs('etc')
     
 # Check if config file is not exist
 if not os.path.isfile('./config.py'):
@@ -22,14 +24,17 @@ if not os.path.isfile('./config.py'):
 ApiKey = ""
 
 # Your minecraft username & profile name
-Username = ""
-Profile = ""
+Username    = ""
+ProfileName = ""
 
 # Enable all kill
-EnableAll = True
+EnableKillAll = True
 
 # Refresh time ( in seconds ) ( default is 30 )
 RefreshTime = 30
+
+# etc
+EnableFairySouls = True
 
 # Kill count enable list ( Leave it empty when EnableAll is True )
 # Example: ['zealot_enderman', 'ruin_wolf']
@@ -42,32 +47,39 @@ EnableKillCount = ['zealot_enderman', 'enderman']""")
 
 # Check if config is valid
 import config as config
-Config = [config.ApiKey, config.Username, config.Profile, 
-        config.EnableKillCount, config.RefreshTime, config.EnableAll]    
-if Config[0]==None:
+
+ApiKey           = config.ApiKey
+Username         = config.Username
+ProfileName      = config.ProfileName
+RefreshTime      = config.RefreshTime
+EnableKillCount  = config.EnableKillCount
+EnableKillAll    = config.EnableKillAll
+EnableFairySouls = config.EnableFairySouls
+
+if ApiKey==None:
     print("Invalid API Key")
     input("Press Enter to continue...")
     exit()
-elif Config[1]==None:
+elif Username==None:
     print("Username can't be empty")
     input("Press Enter to continue...")
     exit()
-elif Config[2]==None:
+elif ProfileName==None:
     print("Profile can't be empty")
     input("Press Enter to continue...")
     exit()
-elif len(Config[3])==0 and Config[5]==False:
+elif len(EnableKillCount)==0 and EnableKillAll==False:
     print("You must choose enable all or enable some")
     input("Press Enter to continue...")
     exit()
-elif type(Config[4])!=int:
+elif type(RefreshTime)!=int:
     print("Refresh time must be number")
     input("Press Enter to continue...")
     exit()
 
 # Check if username is valid and get uuid
 UUID = ""
-r = requests.get("https://api.mojang.com/users/profiles/minecraft/"+Config[1])
+r = requests.get("https://api.mojang.com/users/profiles/minecraft/"+Username)
 status = r.status_code
 if status == 200:
     data = r.json()
@@ -84,7 +96,7 @@ else:
 while True:
     # Check if Hypixel API is limit
     Limit = True
-    r = requests.get("https://api.hypixel.net/key?key="+Config[0]) 
+    r = requests.get("https://api.hypixel.net/key?key="+ApiKey) 
     data = r.json()
     if(data["success"]==False):
         print("[WARNING]: Hypixel API Error, " + data["cause"])
@@ -95,41 +107,59 @@ while True:
         else:
             Limit = False
 
-    # Get Skyblock Profile stats
+    # Get Skyblock Profile stats & Fairy Souls count
     stats = {}
+    fairy_souls_collected = 0
     if Limit == False:
-        r = requests.get("https://api.hypixel.net/skyblock/profiles?key="+Config[0]+"&uuid="+UUID) 
+        r = requests.get("https://api.hypixel.net/skyblock/profiles?key="+ApiKey+"&uuid="+UUID) 
         data = r.json()
         if(data["success"]==False):
             print("[WARNING]: Hypixel API Error, " + data["cause"])
         if(data["success"]==True):
             Profiles = data["profiles"]
             for Profile in Profiles:
-                if Profile["cute_name"] == Config[2]:
+                if Profile["cute_name"] == ProfileName:
+
+                    # Get fairy souls
+                    if ( EnableFairySouls and "fairy_souls_collected" in Profile["members"][UUID] ):
+                        fairy_souls_collected = Profile["members"][UUID]["fairy_souls_collected"]
+                    elif ( EnableFairySouls and "fairy_souls_collected" not in Profile["members"][UUID] ):
+                        print("[ERROR]: API Disable")
+                        input("Press Enter to continue...")
+                        exit()
+
+                    # Get stats
                     if ( "stats" in Profile["members"][UUID] ):
                         stats = Profile["members"][UUID]["stats"]
                     else:
                         print("[ERROR]: API Disable")
                         input("Press Enter to continue...")
                         exit()
+
             if not stats:
-                print("Can't find " + Config[2] + " profile")
+                print("Can't find " + ProfileName + " profile")
                 input("Press Enter to continue...")
                 exit()
     
     # Update kills text file
-    if Config[5]:
+    if EnableKillAll:
         for mob in GetKillMobsList(stats):
             f = open("./kills/"+mob[0]+".txt", "w")
             f.write(str(round(mob[1])))
             f.close()
     else:
-        [x.lower() for x in Config[3]]
+        [x.lower() for x in EnableKillCount]
         for mob in GetKillMobsList(stats):
             if mob[0].replace("kills_", "").lower() in Config[3]:
                 f = open("./kills/"+mob[0]+".txt", "w")
                 f.write(str(round(mob[1])))
                 f.close()
 
+    # Update fairy souls
+    if EnableFairySouls:
+        f = open("./etc/fairy_souls_collected.txt", "w")
+        f.write(str(fairy_souls_collected))
+        f.close()
+
     # Refresh Time
-    time.sleep(Config[4])
+    time.sleep(RefreshTime)
